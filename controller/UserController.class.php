657a8dd604d4d60bb97d00a937642b61991a1772
shangzikun <290917115@qq.com>
2017-10-14 10:41:19 +0800

    test

--- controller/UserController.class.php
+++ controller/UserController.class.php
@@ -4,22 +4,22 @@ class UserController {
 	public function login() {
 		include "./view/user/login.html";
 	}
-	public function dologin() {
+	public function doLogin() {
 		$name = $_POST['username'];
 		$password = $_POST['password'];
 		
 	}
 	public function reg() {
-		include "./view/user/reg.html"
+		include "./view/user/reg.html";
 	}
-	public function doreg() {
+	public function doReg() {
 		$name 	= $_POST['username'];
 		$age 	= $_POST['age'] ? $_POST['age'] : 0;
 		$password = $_POST['password'];
 	}
 	public function logout () {
 		unset($_SESSION['me']);
-		header('Refresh:3,Url=index.php?c=Blog&a=lists');
+		header('Refresh:3,Url=index.php?c=User&a=lists');
 		echo '注销成功';
 		die();
 		}	
