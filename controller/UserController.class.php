d02fa411bba66ef2f067bcef5d2da7c82472a34a
shangzikun <290917115@qq.com>
2017-10-14 10:56:48 +0800

    test

--- controller/UserController.class.php
+++ controller/UserController.class.php
@@ -7,6 +7,8 @@ class UserController {
 	public function doLogin() {
 		$name = $_POST['username'];
 		$password = $_POST['password'];
+		header('Refresh:1,Url=index.php?c=Blog&a=lists');
+		echo '登录成功';
 		
 	}
 	public function reg() {
@@ -16,6 +18,8 @@ class UserController {
 		$name 	= $_POST['username'];
 		$age 	= $_POST['age'] ? $_POST['age'] : 0;
 		$password = $_POST['password'];
+		header('Refresh:1,Url=index.php?c=User&a=login');
+		echo '注册成功，1秒后跳转到list';
 	}
 	public function logout () {
 		unset($_SESSION['me']);
